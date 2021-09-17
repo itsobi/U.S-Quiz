c415546e61aa85d6a03094f8fb13e8ecab2913be
Your Namef <obiobialo@aol.com>
2021-09-17 17:24:12 -0500

    added JS code for buttons

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+
 function checkQuiz() {
-    alert("hello");
+    let q1 = document.getElementsByName("question1");
+    let q2 = document.getElementsByName("question2");
+    let q3 = document.getElementsByName("question3");
+    let display1 = document.getElementById("one-result");
+    let display2 = document.getElementById("two-result");
+    let display3 = document.getElementById("three-result");
+
+   if(q1[1].checked) {
+       display1.innerHTML = "Correct!"
+   }
+   else {
+       display1.innerHTML = "Incorrect! George W. Bush was the 43rd President."
+       display1.style.color = "red";
+   }
+   if (q2[1].checked) {
+       display2.innerHTML = "Correct!";
+   }
+   else {
+       display2.innerHTML = "Incorrect! Alaska is the biggest state in the U.S."
+       display2.style.color = "red";
+   }
+   if (q3[3].checked) {
+       display3.innerHTML = "Correct!"
+   }
+   else {
+       display3.innerHTML = "Incorrect! There are 50 stars on the American Flag."
+       display3.style.color = "red";
+   }
+
+}
+
+function resetQuiz() {
+    window.location.reload();
 }
